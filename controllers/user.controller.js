9decb0805b9c66f4b319cd42ffbb25f08c974a8a
hasibmiraz <hasibmiraz09@gmail.com>
2022-09-30 17:53:04 +0600

    Working on the email

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,9 +1,30 @@
 const User = require('../models/users.schema.js');
+const nodemailer = require('nodemailer');
 
 exports.sendEmail = async (req, res) => {
   const { userID, email, mobileNo } = req.body;
 
-  // const newUser = new User({ userID, email, mobileNo });
+  let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'send.email202209@gmail.com',
+      pass: 'send.email',
+    },
+  });
 
+  const details = await transporter.sendMail({
+    from: 'apiassignment@snellcart.com',
+    to: email,
+    subject: 'Verify your email',
+    html: '<b>Hello world?</b>',
+  });
+
+  transporter.sendMail(details, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Email sent');
+    }
+  });
   return res.status(201).send({ success: true, userID, email, mobileNo });
 };
